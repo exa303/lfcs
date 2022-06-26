43c1a7369c355bf33aca8cb4ecc145a3af238b7f
arijus303 <arijus.zickus@gmail.com>
2022-06-26 14:19:50 +0100

    find

--- scripts_for_system_maintenance/fquota.sh
+++ scripts_for_system_maintenance/fquota.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+#in order to access all directories efectivaly run this program as a root.
+
+if [[ `id -u` -ne 0 ]]; then
+	echo "Please run $0 as ROOT"
+	exit 1
+fi
+
 BORDERLINE=20000 #in Megabytes
 
 for name in $(cat /etc/passwd | cut -d: -f1,2 | awk -F: '$2>99{print $1}'); do
 
 	echo -n "User $name exceeds disk quota. Disk usage is:"
-	# <- change directories based on the system 
+			# <- change directories based on the system 
 	find /home -xdev -user $name -type f -ls | awk '{sum += $7} END {print $sum / (1024*1024) "Mbytes"}'
 
-done | awk "\$9 > $BORDERLINE {print \$0}" #flow if statement
+done | awk "\$9 > $BORDERLINE {print \$0}" #flow if statement, \ becouse it is new awk command
+echo "Done."
 exit 0
