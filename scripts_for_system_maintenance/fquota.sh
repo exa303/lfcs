54275a89df107e8183dbc534f034d092dcc0d289
arijus303 <arijus.zickus@gmail.com>
2022-06-26 15:17:39 +0100

    find

--- scripts_for_system_maintenance/fquota.sh
+++ scripts_for_system_maintenance/fquota.sh
@@ -9,7 +9,7 @@ fi
 
 echo "Runing..."
 
-BORDERLINE=20 #in Megabytes
+BORDERLINE=20000 #in Megabytes
 
 for name in $(cat /etc/passwd | cut -d: -f1,2 | awk -F: '$2>99{print $1}'); do
 
@@ -18,7 +18,6 @@ for name in $(cat /etc/passwd | cut -d: -f1,2 | awk -F: '$2>99{print $1}'); do
 	find /home -xdev -user $name -type f -ls | awk '{sum += $7} END {print sum / (1024*1024)}'
 
 done | awk -F: "\$2 > $BORDERLINE {print \$0}"
-#awk -F: '$2 > $BORDERLINE {print $0}'
-#| awk "\$9 > $BORDERLINE {print \$0}" #flow if statement, \ becouse it is new awk command
+
 echo "Done."
 exit 0
