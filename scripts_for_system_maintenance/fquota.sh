332e8118eb636115ae165ebe3ba6e7629304a261
arijus303 <arijus.zickus@gmail.com>
2022-06-26 14:20:11 +0100

    find

--- scripts_for_system_maintenance/fquota.sh
+++ scripts_for_system_maintenance/fquota.sh
@@ -13,7 +13,7 @@ for name in $(cat /etc/passwd | cut -d: -f1,2 | awk -F: '$2>99{print $1}'); do
 
 	echo -n "User $name exceeds disk quota. Disk usage is:"
 			# <- change directories based on the system 
-	find /home -xdev -user $name -type f -ls | awk '{sum += $7} END {print $sum / (1024*1024) "Mbytes"}'
+	find /home/avx/Desktop -xdev -user $name -type f -ls | awk '{sum += $7} END {print $sum / (1024*1024) "Mbytes"}'
 
 done | awk "\$9 > $BORDERLINE {print \$0}" #flow if statement, \ becouse it is new awk command
 echo "Done."
